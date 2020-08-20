6947d43a116dad225d3ebd49b0daa4ab68085beb
43EVER <i@43ever.me>
2020-08-20 21:27:03 +0800

    比赛代码清理

---
+++ md5sum.bash
@@ -0,0 +1,5 @@
+md5sum ~/imust_racecar/ros_ws/src/simulation/racecar_description/urdf/racecar.urdf.xacro
+md5sum ~/imust_racecar/ros_ws/src/simulation/racecar_description/urdf/sensors/camera.xacro
+md5sum ~/imust_racecar/ros_ws/src/simulation/racecar_description/urdf/sensors/imu.xacro
+md5sum ~/imust_racecar/ros_ws/src/simulation/racecar_description/urdf/sensors/lidar.xacro
+md5sum ~/imust_racecar/ros_ws/src/simulation/racecar_world/worlds/racetrack.world
